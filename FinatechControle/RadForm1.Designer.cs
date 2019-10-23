d58a474c68f38e46ed090246865234ef18a62fbb
HamzaT1992 <tsi.hamza1992@gmail.com>
2019-10-23 11:53:21 +0100

    start

--- FinatechControle/RadForm1.Designer.cs
+++ FinatechControle/RadForm1.Designer.cs
@@ -28,20 +28,251 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.radTreeView2 = new Telerik.WinControls.UI.RadTreeView();
+            this.radPdfViewer2 = new Telerik.WinControls.UI.RadPdfViewer();
+            this.TBFournisseur = new Telerik.WinControls.UI.RadTextBox();
+            this.fournisseurLabel = new Telerik.WinControls.UI.RadLabel();
+            this.dateFactureLabel = new Telerik.WinControls.UI.RadLabel();
+            this.referenceLabel = new Telerik.WinControls.UI.RadLabel();
+            this.NumProjetLabel = new Telerik.WinControls.UI.RadLabel();
+            this.NumBonComLabel = new Telerik.WinControls.UI.RadLabel();
+            this.BULabel = new Telerik.WinControls.UI.RadLabel();
+            this.NumBoiteLabel = new Telerik.WinControls.UI.RadLabel();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.TBDateFacture = new Telerik.WinControls.UI.RadTextBox();
+            this.TBReference = new Telerik.WinControls.UI.RadTextBox();
+            this.TBNumProjet = new Telerik.WinControls.UI.RadTextBox();
+            this.TBNumBonCom = new Telerik.WinControls.UI.RadTextBox();
+            this.TBbu = new Telerik.WinControls.UI.RadTextBox();
+            this.TBNumBoite = new Telerik.WinControls.UI.RadTextBox();
+            this.radPdfViewerNavigator1 = new Telerik.WinControls.UI.RadPdfViewerNavigator();
+            ((System.ComponentModel.ISupportInitialize)(this.radTreeView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radPdfViewer2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TBFournisseur)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fournisseurLabel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateFactureLabel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.referenceLabel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumProjetLabel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumBonComLabel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BULabel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumBoiteLabel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TBDateFacture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TBReference)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TBNumProjet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TBNumBonCom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TBbu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TBNumBoite)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radPdfViewerNavigator1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
             this.SuspendLayout();
             // 
+            // radTreeView2
+            // 
+            this.radTreeView2.Location = new System.Drawing.Point(2, 12);
+            this.radTreeView2.Name = "radTreeView2";
+            this.radTreeView2.Size = new System.Drawing.Size(197, 601);
+            this.radTreeView2.SpacingBetweenNodes = -1;
+            this.radTreeView2.TabIndex = 0;
+            // 
+            // radPdfViewer2
+            // 
+            this.radPdfViewer2.Location = new System.Drawing.Point(206, 44);
+            this.radPdfViewer2.Name = "radPdfViewer2";
+            this.radPdfViewer2.Size = new System.Drawing.Size(679, 569);
+            this.radPdfViewer2.TabIndex = 1;
+            this.radPdfViewer2.ThumbnailsScaleFactor = 0.15F;
+            // 
+            // TBFournisseur
+            // 
+            this.TBFournisseur.Location = new System.Drawing.Point(900, 68);
+            this.TBFournisseur.Name = "TBFournisseur";
+            this.TBFournisseur.Size = new System.Drawing.Size(192, 20);
+            this.TBFournisseur.TabIndex = 2;
+            // 
+            // fournisseurLabel
+            // 
+            this.fournisseurLabel.Location = new System.Drawing.Point(900, 44);
+            this.fournisseurLabel.Name = "fournisseurLabel";
+            this.fournisseurLabel.Size = new System.Drawing.Size(64, 18);
+            this.fournisseurLabel.TabIndex = 3;
+            this.fournisseurLabel.Text = "Fournisseur";
+            // 
+            // dateFactureLabel
+            // 
+            this.dateFactureLabel.Location = new System.Drawing.Point(900, 107);
+            this.dateFactureLabel.Name = "dateFactureLabel";
+            this.dateFactureLabel.Size = new System.Drawing.Size(69, 18);
+            this.dateFactureLabel.TabIndex = 4;
+            this.dateFactureLabel.Text = "Date Facture";
+            // 
+            // referenceLabel
+            // 
+            this.referenceLabel.Location = new System.Drawing.Point(900, 174);
+            this.referenceLabel.Name = "referenceLabel";
+            this.referenceLabel.Size = new System.Drawing.Size(56, 18);
+            this.referenceLabel.TabIndex = 5;
+            this.referenceLabel.Text = "Référence";
+            // 
+            // NumProjetLabel
+            // 
+            this.NumProjetLabel.Location = new System.Drawing.Point(900, 241);
+            this.NumProjetLabel.Name = "NumProjetLabel";
+            this.NumProjetLabel.Size = new System.Drawing.Size(51, 18);
+            this.NumProjetLabel.TabIndex = 6;
+            this.NumProjetLabel.Text = "N° Projet";
+            // 
+            // NumBonComLabel
+            // 
+            this.NumBonComLabel.Location = new System.Drawing.Point(900, 302);
+            this.NumBonComLabel.Name = "NumBonComLabel";
+            this.NumBonComLabel.Size = new System.Drawing.Size(118, 18);
+            this.NumBonComLabel.TabIndex = 7;
+            this.NumBonComLabel.Text = "N° Bon de Commande";
+            // 
+            // BULabel
+            // 
+            this.BULabel.Location = new System.Drawing.Point(900, 364);
+            this.BULabel.Name = "BULabel";
+            this.BULabel.Size = new System.Drawing.Size(20, 18);
+            this.BULabel.TabIndex = 8;
+            this.BULabel.Text = "BU";
+            // 
+            // NumBoiteLabel
+            // 
+            this.NumBoiteLabel.Location = new System.Drawing.Point(900, 429);
+            this.NumBoiteLabel.Name = "NumBoiteLabel";
+            this.NumBoiteLabel.Size = new System.Drawing.Size(48, 18);
+            this.NumBoiteLabel.TabIndex = 9;
+            this.NumBoiteLabel.Text = "N° Boite";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // TBDateFacture
+            // 
+            this.TBDateFacture.Location = new System.Drawing.Point(900, 131);
+            this.TBDateFacture.Name = "TBDateFacture";
+            this.TBDateFacture.Size = new System.Drawing.Size(192, 20);
+            this.TBDateFacture.TabIndex = 11;
+            // 
+            // TBReference
+            // 
+            this.TBReference.Location = new System.Drawing.Point(900, 198);
+            this.TBReference.Name = "TBReference";
+            this.TBReference.Size = new System.Drawing.Size(192, 20);
+            this.TBReference.TabIndex = 12;
+            // 
+            // TBNumProjet
+            // 
+            this.TBNumProjet.Location = new System.Drawing.Point(900, 265);
+            this.TBNumProjet.Name = "TBNumProjet";
+            this.TBNumProjet.Size = new System.Drawing.Size(192, 20);
+            this.TBNumProjet.TabIndex = 13;
+            // 
+            // TBNumBonCom
+            // 
+            this.TBNumBonCom.Location = new System.Drawing.Point(900, 326);
+            this.TBNumBonCom.Name = "TBNumBonCom";
+            this.TBNumBonCom.Size = new System.Drawing.Size(192, 20);
+            this.TBNumBonCom.TabIndex = 14;
+            // 
+            // TBbu
+            // 
+            this.TBbu.Location = new System.Drawing.Point(900, 388);
+            this.TBbu.Name = "TBbu";
+            this.TBbu.Size = new System.Drawing.Size(192, 20);
+            this.TBbu.TabIndex = 15;
+            // 
+            // TBNumBoite
+            // 
+            this.TBNumBoite.Location = new System.Drawing.Point(900, 453);
+            this.TBNumBoite.Name = "TBNumBoite";
+            this.TBNumBoite.Size = new System.Drawing.Size(192, 20);
+            this.TBNumBoite.TabIndex = 16;
+            // 
+            // radPdfViewerNavigator1
+            // 
+            this.radPdfViewerNavigator1.AssociatedViewer = this.radPdfViewer2;
+            this.radPdfViewerNavigator1.Location = new System.Drawing.Point(239, 0);
+            this.radPdfViewerNavigator1.Name = "radPdfViewerNavigator1";
+            this.radPdfViewerNavigator1.Size = new System.Drawing.Size(610, 38);
+            this.radPdfViewerNavigator1.TabIndex = 17;
+            // 
             // RadForm1
-            //
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1122, 625);
+            this.Controls.Add(this.radPdfViewerNavigator1);
+            this.Controls.Add(this.TBNumBonCom);
+            this.Controls.Add(this.TBNumBoite);
+            this.Controls.Add(this.TBbu);
+            this.Controls.Add(this.TBNumProjet);
+            this.Controls.Add(this.TBReference);
+            this.Controls.Add(this.TBDateFacture);
+            this.Controls.Add(this.NumBoiteLabel);
+            this.Controls.Add(this.BULabel);
+            this.Controls.Add(this.NumBonComLabel);
+            this.Controls.Add(this.NumProjetLabel);
+            this.Controls.Add(this.referenceLabel);
+            this.Controls.Add(this.dateFactureLabel);
+            this.Controls.Add(this.fournisseurLabel);
+            this.Controls.Add(this.TBFournisseur);
+            this.Controls.Add(this.radPdfViewer2);
+            this.Controls.Add(this.radTreeView2);
             this.Name = "RadForm1";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
             this.Text = "RadForm1";
+            this.Load += new System.EventHandler(this.RadForm1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.radTreeView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radPdfViewer2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TBFournisseur)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fournisseurLabel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateFactureLabel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.referenceLabel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumProjetLabel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumBonComLabel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BULabel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumBoiteLabel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TBDateFacture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TBReference)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TBNumProjet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TBNumBonCom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TBbu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TBNumBoite)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radPdfViewerNavigator1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private Telerik.WinControls.UI.RadTreeView radTreeView1;
+        private Telerik.WinControls.UI.RadPdfViewer radPdfViewer1;
+        private Telerik.WinControls.UI.RadTreeView radTreeView2;
+        private Telerik.WinControls.UI.RadPdfViewer radPdfViewer2;
+        private Telerik.WinControls.UI.RadTextBox TBFournisseur;
+        private Telerik.WinControls.UI.RadLabel fournisseurLabel;
+        private Telerik.WinControls.UI.RadLabel dateFactureLabel;
+        private Telerik.WinControls.UI.RadLabel referenceLabel;
+        private Telerik.WinControls.UI.RadLabel NumProjetLabel;
+        private Telerik.WinControls.UI.RadLabel NumBonComLabel;
+        private Telerik.WinControls.UI.RadLabel BULabel;
+        private Telerik.WinControls.UI.RadLabel NumBoiteLabel;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private Telerik.WinControls.UI.RadTextBox TBDateFacture;
+        private Telerik.WinControls.UI.RadTextBox TBReference;
+        private Telerik.WinControls.UI.RadTextBox TBNumProjet;
+        private Telerik.WinControls.UI.RadTextBox TBNumBonCom;
+        private Telerik.WinControls.UI.RadTextBox TBbu;
+        private Telerik.WinControls.UI.RadTextBox TBNumBoite;
+        private Telerik.WinControls.UI.RadPdfViewerNavigator radPdfViewerNavigator1;
     }
 }
--- FinatechControle/RadForm1.cs
+++ FinatechControle/RadForm1.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -16,6 +19,100 @@ namespace FinatechControle
         public RadForm1()
         {
             InitializeComponent();
+            //var Boites = new RadTreeNode();
+            //Boites.Text = "Boites";
+            radTreeView2.SelectedNodeChanged += Node_Changed;
+        }
+
+        private void RadForm1_Load(object sender, EventArgs e)
+        {
+            var constr = ConfigurationManager.ConnectionStrings["StrCon"].ConnectionString;
+            using (SqlConnection cnn = new SqlConnection(constr))
+            {
+                cnn.Open();
+                string rqtNumBoites = "select distinct Numboite from DossiersIndexes where Type='Achat/FOURNISSEUR'";
+
+
+                SqlDataAdapter da = new SqlDataAdapter(rqtNumBoites, cnn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                foreach (DataRow row in dt.Rows)
+                {
+                    RadTreeNode boite = new RadTreeNode();
+                    var numBoite = row["Numboite"].ToString();
+                    boite.Text = "Boite " + numBoite;
+                    boite.Value = numBoite;
+                    foreach (DataRow item in getDocs(numBoite).Rows)
+                    {
+                        RadTreeNode doc = new RadTreeNode();
+                        var doss = item["NomDossier"].ToString();
+                        var xmlpath = item["CheminPDF"].ToString();
+                        //var pdf = doss.Split('\\')[1];
+                        var path = Path.GetDirectoryName(xmlpath) + "\\" + doss;
+                        doc.Text = doss;
+                        doc.Value = path;
+                        boite.Nodes.Add(doc);
+                    }
+                    //Boites.Nodes.Add(boite);
+                    radTreeView2.Nodes.Add(boite);
+                }
+
+            }
+        }
+
+        private DataTable getDocs(string numboite)
+        {
+            var constr = ConfigurationManager.ConnectionStrings["StrCon"].ConnectionString;
+            using (SqlConnection cnn = new SqlConnection(constr))
+            {
+                cnn.Open();
+                string reqdocs = "select * from DossiersIndexes where Numboite = "+numboite;
+
+
+                SqlDataAdapter da = new SqlDataAdapter(reqdocs, cnn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                return dt;
+                
+            }
+        }
+
+        private void Node_Changed(object sender, RadTreeViewEventArgs e)
+        {
+            if (e.Node.Level == 1)
+            {
+                radPdfViewer2.LoadDocument(e.Node.Value.ToString());
+                var row = getIndexs(e.Node.Text).Rows[0];
+                TBFournisseur.Text = row["Fournisseur"].ToString();
+                TBDateFacture.Text = row["DateFacture"].ToString();
+                TBReference.Text = row["Reference"].ToString();
+                TBNumProjet.Text = row["NumProjet"].ToString();
+                TBNumBonCom.Text = row["NumBonCommande"].ToString();
+                TBbu.Text = row["BU"].ToString();
+                TBNumBoite.Text = row["NumBoite"].ToString();
+            }
+        }
+
+        private DataTable getIndexs(string nomDoss)
+        {
+            var constr = ConfigurationManager.ConnectionStrings["StrCon"].ConnectionString;
+            using (SqlConnection cnn = new SqlConnection(constr))
+            {
+                cnn.Open();
+                string reqdocs = "select * from Achat where NomDossier = '" + nomDoss+"'";
+
+
+                SqlDataAdapter da = new SqlDataAdapter(reqdocs, cnn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                return dt;
+
+            }
+        }
+
+        private void radLabel1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
